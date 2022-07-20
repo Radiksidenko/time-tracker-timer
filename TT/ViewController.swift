d5edb24756e079f54e496fbac7eb3f4d4e8f93fe
Radomyr Sidenko <radik-sidehnko@ukr.net>
2022-07-20 11:24:08 +0200

    Initial Commit

---
+++ TT/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TT
+//
+//  Created by Radomyr Sidenko on 20.07.2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
